bd62b2bcde9d69feb2868db58aa6ed9a706ac3cc
baranbicher <baranbicer0103@outlook.com>
2023-06-05 10:58:21 +0300

    sql

---
+++ postgres-sql-Project/Project-1/project-q4.sql
@@ -0,0 +1,5 @@
+--customer tablosunda bulunan first_name sütunundaki değeri 'Mary' olan müşterinin last_name sütunundaki değeri nedir?
+
+SELECT * FROM customer WHERE first_name = 'Mary';
+
+--last_name='Smith'
